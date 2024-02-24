316614d11d3a4cdc1ff48c168394291ac365c308
hawkes-tamu <hawkes@tamu.edu>
2024-02-24 13:51:12 -0600

    stole more of devons code

--- drowsy_script.py
+++ drowsy_script.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import sys
 import tensorflow as tf
-
+import tensorflow.keras
 from sklearn.model_selection import train_test_split
 
 EPOCHS = 100
@@ -44,3 +44,76 @@ def main():
         filename = sys.argv[2]
         model.save(filename)
         print(f"Model saved to {filename}.")
+
+
+def load_data(data_dir):
+    """
+    Load image data from directory `data_dir`.
+
+    Assume `data_dir` has one directory named after each category, numbered
+    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
+    number of image files.
+
+    Return tuple `(images, labels)`. `images` should be a list of all
+    of the images in the data directory, where each image is formatted as a
+    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
+    be a list of integer labels, representing the categories for each of the
+    corresponding `images`.
+    """
+
+    # initialize empty image and label lists for returning
+    images = []
+    labels = []
+
+    # read data from each folder
+    for i in range(NUM_CATEGORIES):
+
+        # add every image from folder to list
+        for file in os.listdir(os.path.join(data_dir, str(i))):
+            images.append(cv2.resize(cv2.imread(f'{os.path.join(data_dir, str(i), file)}', 1), (IMG_WIDTH, IMG_HEIGHT)))
+            labels.append(i)
+
+    return (images, labels)
+
+
+def get_model():
+    """
+    Returns a compiled convolutional neural network model. Assume that the
+    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
+    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    """
+
+    model = tf.keras.models.Sequential([
+
+        # add a convolutional layer with 32 filters an a 3x3 kernal
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+
+        # add a max-pooling layer with 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # flatten data
+        tf.keras.layers.Flatten(),
+
+        # add a hidden layer with relu activation and dropout
+        tf.keras.layers.Dense(256, activation="relu"),
+        tf.keras.layers.Dense(256, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+
+        # add an output later with output units for each category
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+    return model
+
+
+if __name__ == "__main__":
+    main()
