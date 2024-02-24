3d3e61eef5cef36d7fb63b9df7139610a082ed97
hawkes-tamu <hawkes@tamu.edu>
2024-02-24 13:09:11 -0600

    beginnings of main function, also changed epoch number and image size

---
+++ drowsy_script.py
@@ -0,0 +1,46 @@
+import cv2
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+EPOCHS = 100
+IMG_WIDTH = 227 # images are 227 pixels by 227 pixels
+IMG_HEIGHT = 227
+NUM_CATEGORIES = 43
+TEST_SIZE = 0.4
+
+def main():
+
+    ### FIX ME
+    # -------------------------------------------------
+    # Check command-line arguments
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+
+    # Get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
+    # --------------------------------------------------
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
+
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
+
+    # Save model to file
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
