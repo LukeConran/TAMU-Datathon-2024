8a2e5f15ebd03be4cdfd71cc77437b544573004e
iSkewb <62757622+iSkewb@users.noreply.github.com>
2024-02-25 14:06:47 -0600

    Update Devon4.py

--- Devon4.py
+++ Devon4.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import tensorflow.keras
 from sklearn.model_selection import train_test_split
 
-EPOCHS = 1
+EPOCHS = 5
 IMG_WIDTH = 227 # images are 227 pixels by 227 pixels
 IMG_HEIGHT = 227
 NUM_CATEGORIES = 2
@@ -20,7 +20,7 @@ def main():
     # -------------------------------------------------
     # Check command-line arguments
     if len(sys.argv) not in [3, 4]:
-        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+        sys.exit("Usage: python traffic.py data_directory [outputModel.h5] (optional)[inputModel.h5]")
 
     # Get image arrays and labels for all image files
     images, labels = load_data(sys.argv[1])
@@ -41,10 +41,12 @@ def main():
     model.evaluate(x_test,  y_test, verbose=2)
 
     # Save model to file
-    if len(sys.argv) == 3:
+    if len(sys.argv) >= 3:
         filename = sys.argv[2]
         model.save(filename)
         print(f"Model saved to {filename}.")
+        # with open("accuracies.txt", "w") as file:
+            # file.write(f"Accuracy of {filename} = {model.accuracy}\n")
 
 
 def load_data(data_dir):
@@ -72,7 +74,9 @@ def load_data(data_dir):
     # add every image from folder to list
     for i, file in enumerate(os.listdir(os.path.join(data_dir, "drowsy"))):
         # if not (i % 50 == 0): continue
-        images.append(cv2.resize(cv2.imread(f'{os.path.join(data_dir, "drowsy", file)}', 1), (IMG_WIDTH, IMG_HEIGHT)))
+        image = cv2.resize(cv2.imread(f'{os.path.join(data_dir, "drowsy", file)}', cv2.IMREAD_GRAYSCALE), (IMG_WIDTH, IMG_HEIGHT))
+        image.resize((IMG_WIDTH),(IMG_HEIGHT))
+        images.append(image)
         labels.append(1)
         # f = open("smallerData/drowsy/" + file, "w")
         # f.close()
@@ -80,7 +84,9 @@ def load_data(data_dir):
 
     for i, file in enumerate(os.listdir(os.path.join(data_dir, "nonDrowsy"))):
         # if not (i % 50 == 0): continue
-        images.append(cv2.resize(cv2.imread(f'{os.path.join(data_dir, "nonDrowsy", file)}', 1), (IMG_WIDTH, IMG_HEIGHT)))
+        image = cv2.resize(cv2.imread(f'{os.path.join(data_dir, "nonDrowsy", file)}', cv2.IMREAD_GRAYSCALE),(IMG_WIDTH, IMG_HEIGHT))
+        image.resize((IMG_WIDTH), (IMG_HEIGHT))
+        images.append(image)
         labels.append(0)
         # f = open("smallerData/nonDrowsy/" + file, "w")
         # f.close()
@@ -103,13 +109,14 @@ def get_model():
             new_model = tf.keras.models.load_model(sys.argv[3])
             return new_model
     except:
+        print("No model provided. Creating a new model.")
         pass
 
     model = tf.keras.models.Sequential([
 
-        # add a convolutional layer with 32 filters an a 3x3 kernal
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
-        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        # add a convolutional layer with 32 filters on a 3x3 kernel
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 1)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(IMG_WIDTH, IMG_HEIGHT, 1)),
 
         # add a max-pooling layer with 2x2 pool size
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
