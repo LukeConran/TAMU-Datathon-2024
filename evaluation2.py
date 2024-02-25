d929ac6afb13bd6528448214d708475c8dd667e1
hawkes-tamu <161142443+hawkes-tamu@users.noreply.github.com>
2024-02-25 11:43:05 -0600

    Create evaluation2.py

---
+++ evaluation2.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+if len(sys.argv) == 3:
+    loaded_model = tf.keras.models.load_model(sys.argv[1])
+    image_name = sys.argv[2]
+
+    # # load image via tf.io
+    # img = tf.io.read_file(image_name)
+    #
+    # # convert to tensor (specify 3 channels explicitly since png files contains additional alpha channel)
+    # # set the dtypes to align with pytorch for comparison since it will use uint8 by default
+    # tensor = tf.io.decode_image(img, channels=3, dtype=tf.dtypes.float32)
+    # # (x, y, 3)
+    #
+    # # resize tensor to 227 x 227
+    # tensor = tf.image.resize(tensor, [227, 227])
+    # # (227, 227, 3)
+    images = []
+    images.append(cv2.resize(cv2.imread(image_name, 1), (227, 227)))
+
+    # add another dimension at the front to get NHWC shape
+    input_tensor = tf.expand_dims(images, axis=0)
+    # Interpret the evaluation metrics
+    accuracy = loaded_model.evaluate(input_tensor)
+    print(accuracy)
+    # if accuracy >= 0.5:  # adjust the threshold as needed
+    #     print("Image fits the criteria")
+    # else:
+    #     print("Image does not fit the criteria")
